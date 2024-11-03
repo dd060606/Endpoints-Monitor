491bc4fc4a1bc60e476991b1cfb51c8db0f07f05
dd060606 <dd06.developpement@gmail.com>
2024-11-03 11:40:12 +0100

    feat: filter out useless extensions + fix: cookies bug

--- endpoints-monitor.py
+++ endpoints-monitor.py
@@ -62,7 +62,7 @@ def extract_js_files(url: str, headers = {}, cookies = {}) -> list[str]:
         # Get the full URL of the JavaScript file
         js_url = urljoin(url, script['src'])
         js_files.append(js_url)
-        
+    
     return js_files
 
 
@@ -76,8 +76,7 @@ def get_file_content(url: str, headers = {}, cookies = {}) -> str:
         print(f"Error fetching the file: {e}")
         return ""
 
-def extract_endpoints_from_js(js_content: str) -> list[str]:
-
+def extract_endpoints_from_js(js_content: str, filter_enabled: bool = False) -> list[str]:
     # Regular expression to capture endpoints
     # This regex captures full URLs, and paths that look like endpoints (e.g., "/api/", "/v1/")
     endpoint_pattern = re.compile(regex_str, re.VERBOSE | re.IGNORECASE)
@@ -85,8 +84,13 @@ def extract_endpoints_from_js(js_content: str) -> list[str]:
     # Find all matches for endpoints
     endpoints = re.findall(endpoint_pattern, js_content)
 
-    # Extract the first capturing group from each match
-    endpoints = [match[0] for match in endpoints if match[0]]
+    if filter_enabled:
+        # Filter out common file extensions if enabled
+        disallowed_extensions = (".png", ".jpg", ".jpeg", ".gif", ".svg",".webp", ".ico", ".css", ".json")
+        endpoints = [match[0] for match in endpoints if match[0] and not match[0].endswith(disallowed_extensions)]
+    else:
+        # Extract the first capturing group from each match
+        endpoints = [match[0] for match in endpoints if match[0]]
 
     # Remove duplicates by converting to a set, then back to a list
     endpoints = list(set(endpoints))
@@ -220,16 +224,19 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--cookies", help="Cookies to include in requests (e.g, 'cookie1=aaaaaa; cookie2=bbbbbbbb')", default="")
     parser.add_argument("-o", "--output", help="Output directory to save the found endpoints", default="endpoints-output/")
     parser.add_argument("-w", "--discord-webhook", help="Discord webhook URL to notify about new endpoints", default="")
+    parser.add_argument("-f", "--filter", help="Filter out common file extensions (e.g., .png, .jpg, .json, .svg)", action="store_true")
+
 
     args = parser.parse_args()
+
     # Remove trailing slash from the URL
     if args.input[-1:] == "/":
         args.input = args.input[:-1]
     
 
     # Convert headers and cookies to a dictionary
-    headers = dict(x.split(': ') for x in args.headers.split('; ')) if args.headers else {}
-    cookies = dict(x.split('=') for x in args.cookies.split('; ')) if args.cookies else {}
+    headers = dict(x.split(': ') for x in args.headers.split(';')) if args.headers else {}
+    cookies = dict(x.split('=', 1) for x in args.cookies.split(';') if '=' in x) if args.cookies else {}
     # Create output directory
     os.makedirs(args.output, exist_ok=True)
     latest_endpoints_dir = os.path.join(args.output, "latest_endpoints")
@@ -246,7 +253,7 @@ if __name__ == "__main__":
         for js_file in js_files:
             js_content = get_file_content(js_file, headers, cookies)
             filename = urlparse(js_file).path
-            endpoints[filename] = extract_endpoints_from_js(js_content)
+            endpoints[filename] = extract_endpoints_from_js(js_content, args.filter)
 
         hostname = urlparse(url).hostname
         latest_endpoints_file = os.path.join(latest_endpoints_dir, f"{hostname.replace(".", "-")}.txt")
